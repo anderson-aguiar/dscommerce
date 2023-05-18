a309d13886d11fc17c356225f6cc0366bc5a97a6
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-05-18 08:42:10 -0300

    User

---
+++ src/test/java/com/amtechnology/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amtechnology.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
