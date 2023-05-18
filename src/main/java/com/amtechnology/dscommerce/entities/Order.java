790d4632a5e4affb51fb9d5934e1a09420834ee9
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-05-18 09:26:31 -0300

    Payment

--- src/main/java/com/amtechnology/dscommerce/entities/Order.java
+++ src/main/java/com/amtechnology/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.amtechnology.dscommerce.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,31 +10,38 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private OrderStatus status;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	public Order() {}
 
-	public Order(Long id, Instant moment, OrderStatus status) {
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
+	public Order() {
+	}
+
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.status = status;
+		this.client = client;
+		this.payment = payment;
 	}
 
 	public Long getId() {
@@ -59,7 +67,21 @@ public class Order {
 	public void setStatus(OrderStatus status) {
 		this.status = status;
 	}
-	
-	
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 }
