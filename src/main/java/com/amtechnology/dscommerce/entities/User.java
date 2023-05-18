a58b02b80c437f338034b5d7eb31b37f84e1f72b
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-05-18 10:29:54 -0300

    Product and Category

--- src/main/java/com/amtechnology/dscommerce/entities/User.java
+++ src/main/java/com/amtechnology/dscommerce/entities/User.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,8 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String phone;
 	private LocalDate birthDate;
