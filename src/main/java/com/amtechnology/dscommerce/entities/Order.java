32c0f28233480016efe9df6771b381f4a6eecf7f
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-05-25 21:11:46 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/amtechnology/dscommerce/entities/Order.java
+++ src/main/java/com/amtechnology/dscommerce/entities/Order.java
@@ -1,6 +1,9 @@
 package com.amtechnology.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -31,6 +35,9 @@ public class Order {
 
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 	}
@@ -84,4 +91,10 @@ public class Order {
 		this.payment = payment;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
 }
