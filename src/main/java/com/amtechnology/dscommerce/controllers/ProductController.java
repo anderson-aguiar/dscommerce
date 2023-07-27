71d96cfdbaaa5a27f08aec280898b20f3dab61f4
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-07-27 15:34:52 -0300

    Access Control

--- src/main/java/com/amtechnology/dscommerce/controllers/ProductController.java
+++ src/main/java/com/amtechnology/dscommerce/controllers/ProductController.java
@@ -2,10 +2,13 @@ package com.amtechnology.dscommerce.controllers;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,8 +22,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.amtechnology.dscommerce.dto.ProductDTO;
 import com.amtechnology.dscommerce.services.ProductService;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -40,6 +41,7 @@ public class ProductController {
 		Page<ProductDTO> dto = service.findAll(name, pageable);
 		return ResponseEntity.ok(dto);
 	}
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
@@ -49,11 +51,13 @@ public class ProductController {
 		
 		//retorna na requisição o codigo 201 que é de recurso criado
 	}
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id,@Valid @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
