1f4005af732a612e759954daccbc87120aacc249
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-08-01 15:15:03 -0300

    Get users logged

--- src/main/java/com/amtechnology/dscommerce/services/UserService.java
+++ src/main/java/com/amtechnology/dscommerce/services/UserService.java
@@ -1,11 +1,14 @@
 package com.amtechnology.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.amtechnology.dscommerce.dto.UserDTO;
 import com.amtechnology.dscommerce.entities.User;
 import com.amtechnology.dscommerce.repositories.UserRepository;
 
@@ -17,11 +20,27 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		User user = repository.findByEmail(username);
 		if (user == null) {
 			throw new UsernameNotFoundException("Email not found");
 		}
 		return user;
+	}
+
+	protected User authenticated() {
+		try {
+			String username = SecurityContextHolder.getContext().getAuthentication().getName();
+			return repository.findByEmail(username);
+		} catch (Exception e) {
+			throw new UsernameNotFoundException("Invalid User");
+		}
+
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User entity = authenticated();
+		return new UserDTO(entity);
 	}
 }
